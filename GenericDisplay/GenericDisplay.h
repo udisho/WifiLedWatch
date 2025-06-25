99a74153a2416bd01304006ef3bd9d7f492fbd5b
udi.shorer <udisho90@gmail.com>
2025-06-25 08:19:28 +0300

    Feature Add CONN for display

--- GenericDisplay/GenericDisplay.h
+++ GenericDisplay/GenericDisplay.h
@@ -7,6 +7,7 @@ public:
     virtual void RunTestLeds(void);
     virtual void ShowDigits(int numToShow);
     virtual void ChangeColor(void);
+    virtual void ShowCONN(void);
 };
 
 #endif // _GENERIC_DISPLAY_
